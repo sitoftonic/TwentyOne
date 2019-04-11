6271cccee87bfc889c4377746b59b249a96a869b
sitoftonic <sitoftonic@gmail.com>
2019-04-11 22:13:12 +0200

    #14
    Added changePassword error controls

--- app/src/main/java/com/example/twentyone/restapi/callback/AccountAPICallBack.java
+++ app/src/main/java/com/example/twentyone/restapi/callback/AccountAPICallBack.java
@@ -1,16 +1,26 @@
 package com.example.twentyone.restapi.callback;
 
-import com.example.twentyone.model.data.User;
-
 public interface AccountAPICallBack extends RestAPICallBack {
     // Este método se llama cuando se ha producido el cambio de contraseña a través de I forgot my psswd
     void onResetPasswordFinish();
+
     // Este método se llama cuando el usuario no recuerda la contraseña y se le ha enviado el correo
     void onResetPasswordInit();
+
     // Este método se llama cuando se cambia la contraseña desde el perfil del usuario
     void onChangePassword();
+
+    // Este método se llama cuando se intenta registrar un usuario con un username que ya existe
     void onUsernameFailed();
+
+    // Este método se llama cuando se intenta registrar un usuario con un email que ya existe
     void onEmailFailed();
+
+
+    void onPasswordDontMatch();
+
+
+    /* Estos métodos son heredados de la interface RestAPICallBack */
     void onFailure();
     void onSuccess();
 }
