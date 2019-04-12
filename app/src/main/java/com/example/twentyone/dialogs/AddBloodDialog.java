6f07432ad6801e9e5e54e36bc2cc0e1736d9f1d6
wsx <belaev91@gmail.com>
2019-04-12 17:00:05 +0200

    Created initView and saveBlood methods, ready for API Request #24

--- app/src/main/java/com/example/twentyone/dialogs/AddBloodDialog.java
+++ app/src/main/java/com/example/twentyone/dialogs/AddBloodDialog.java
@@ -3,32 +3,50 @@ package com.example.twentyone.dialogs;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.example.twentyone.R;
 import com.example.twentyone.model.data.BloodPressure;
 import com.example.twentyone.restapi.callback.BloodAPICallBack;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatCheckBox;
 import androidx.fragment.app.DialogFragment;
 
 public class AddBloodDialog extends DialogFragment implements BloodAPICallBack {
 
+    private TextInputLayout date_input;
+    private TextInputEditText date_text;
+
+    private TextInputLayout systolic_input;
+    private TextInputEditText systolic_text;
+
+    private TextInputLayout diastolic_input;
+    private TextInputEditText diastolic_text;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        //iew view2 = getLayoutInflater().inflate(R.layout.dialog_add_points, (ViewGroup) getView());
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        final View view = inflater.inflate(R.layout.dialog_add_blood, null);
+        initView(view);
 
         builder.setTitle(R.string.add_blood_title)
-                .setView(R.layout.dialog_add_blood)
+                .setView(view)
                 .setPositiveButton(R.string.add_blood_save, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        saveBlood();
                     }
                 })
                 .setNegativeButton(R.string.add_blood_cancel, new DialogInterface.OnClickListener() {
@@ -38,6 +56,26 @@ public class AddBloodDialog extends DialogFragment implements BloodAPICallBack {
         // Create the AlertDialog object and return it
 
         return builder.create();
+    }
+
+    private void initView(View view) {
+
+        date_input = view.findViewById(R.id.add_blood_date_input);
+        date_text = view.findViewById(R.id.add_blood_date_text);
+        systolic_input = view.findViewById(R.id.add_blood_systolic_input);
+        systolic_text = view.findViewById(R.id.add_blood_systolic_text);
+        diastolic_input = view.findViewById(R.id.add_blood_diastolic_input);
+        diastolic_text = view.findViewById(R.id.add_blood_diastolic_text);
+    }
+
+    private void saveBlood() {
+
+        final String date = date_text.getText().toString();
+        final String systolic = systolic_text.getText().toString();
+        final String diastolic = diastolic_text.getText().toString();
+
+        Log.i("ADD- BLOOD", "Date: " + date + " Systolic: " + systolic + " Diastolic: " + diastolic);
+
     }
 
     @Override
