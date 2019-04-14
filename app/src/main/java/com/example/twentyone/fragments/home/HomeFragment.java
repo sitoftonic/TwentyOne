159ea43eaf1b5a0b91007601ee7071906498776b
xoselolo <1a09lopxose@gmail.com>
2019-04-14 19:31:27 +0200

    #30 - Gráfica points sin request + color de relleno:)

--- app/src/main/java/com/example/twentyone/fragments/home/HomeFragment.java
+++ app/src/main/java/com/example/twentyone/fragments/home/HomeFragment.java
@@ -1,11 +1,12 @@
 package com.example.twentyone.fragments.home;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.twentyone.R;
@@ -13,10 +14,24 @@ import com.example.twentyone.dialogs.AddBloodDialog;
 import com.example.twentyone.dialogs.AddPointsDialog;
 import com.example.twentyone.dialogs.AddWeightDialog;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
@@ -108,13 +123,154 @@ public class HomeFragment extends Fragment {
     }
 
     private void getPoints() {
-
+        // Temporal
+        ArrayList<Integer> points = new ArrayList<>();
+        points.add(1);
+        points.add(3);
+        points.add(2);
+        points.add(3);
+        points.add(1);
+        points.add(0);
+        points.add(1);
+        initChartPoints(points);
     }
 
-    private void initChartPoints() {
+    private void initChartPoints(ArrayList<Integer> points) {
         chartPoints = view.findViewById(R.id.chartPoints);
 
 
+        {   // // Chart Style // //
+            // background color
+            chartPoints.setBackgroundColor(Color.WHITE);
+
+            // disable description text
+            chartPoints.getDescription().setEnabled(false);
+
+            // enable touch gestures
+            chartPoints.setTouchEnabled(true);
+
+            // enable scaling and dragging
+            chartPoints.setDragEnabled(true);
+            chartPoints.setScaleEnabled(true);
+
+            // force pinch zoom along both axis
+            chartPoints.setPinchZoom(true);
+        }
+
+        XAxis xAxis;
+        {   // // X-Axis Style // //
+            xAxis = chartPoints.getXAxis();
+
+            // vertical grid lines
+            xAxis.enableGridDashedLine(10f, 10f, 0f);
+        }
+
+        YAxis yAxis;
+        {   // // Y-Axis Style // //
+            yAxis = chartPoints.getAxisLeft();
+
+            // disable dual axis (only use LEFT axis)
+            chartPoints.getAxisRight().setEnabled(false);
+
+            // horizontal grid lines
+            yAxis.enableGridDashedLine(10f, 10f, 0f);
+
+            // axis range
+            yAxis.setAxisMaximum(3);
+            yAxis.setAxisMinimum(0);
+        }
+
+
+        // add data
+        setDataPoints(points);
+
+
+        // get the legend (only possible after setting data)
+        Legend l = chartPoints.getLegend();
+
+        // draw legend entries as lines
+        l.setForm(Legend.LegendForm.LINE);
+
+        LegendEntry[] entries = new LegendEntry[1];
+        entries[0] = new LegendEntry();
+        entries[0].label = "Points for last 7 days";
+        l.setCustom(entries);
+    }
+
+    private void setDataPoints(ArrayList<Integer> points) {
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < points.size(); i++) {
+            values.add(new Entry(i, points.get(i)));
+        }
+
+        LineDataSet set1;
+
+        if (chartPoints.getData() != null &&
+                chartPoints.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) chartPoints.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            set1.notifyDataSetChanged();
+            chartPoints.getData().notifyDataChanged();
+            chartPoints.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setDrawIcons(false);
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f);
+
+            // black lines and points
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+
+            // line thickness and point size
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false);
+
+            // customize legend entry
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            // text size of values
+            set1.setValueTextSize(9f);
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+
+            // set the filled area
+            set1.setDrawFilled(true);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return chartPoints.getAxisLeft().getAxisMinimum();
+                }
+            });
+
+            // set color of filled area
+            if (Utils.getSDKInt() >= 18) {
+                // drawables only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(this.getContext(), R.drawable.fade_blue);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.BLACK);
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1); // add the data sets
+
+            // create a data object with the data sets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            chartPoints.setData(data);
+        }
     }
 
     private void initChartBlood() {
@@ -126,6 +282,10 @@ public class HomeFragment extends Fragment {
     private void initChartWeight() {
         chartPoints = view.findViewById(R.id.chartPoints);
 
-
     }
+
+    // TODO: OnDataRequiredFailed para mostrar los chips y no los gráficos
+    // TODO: OnPointsRequiredSuccess para mostrar chart de puntos y esconder chip
+    // TODO: OnBloodRequiredSuccess para mostrar chart de blood y esconder chip
+    // TODO: OnWeightRequiredSuccess para mostrar chart de weight y esconder chip
 }
